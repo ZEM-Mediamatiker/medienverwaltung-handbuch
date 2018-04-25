79d1666d064c1ef254b84765e5b3f7eb3fa8eee8
joelluethi <joel.luethi@zem.ch>
2018-04-25 15:54:03 +0200

    Responsive Navigation
    Bug fixing and mobile sidebar navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,40 @@
+// SET THE FOOTER FIXED-BOTTOM WHEN THE WINDOW-HEIGHT IS BIGGER THAN THE BODY
+
+function fixFooter() {
+        if ($('html').outerHeight()<$(window).outerHeight()) {
+            $('.footer').addClass('fixed-footer');
+        }
+        else {
+            $('.footer').removeClass('fixed-footer');
+       }    
+}
+
+// SWITCH BETWEEN CONTAINER AND CONTAINER-FLUID WHEN RESIZING THE BROWSERWINDOW
+    
+function containerSwitch() {
+    
+    let $s = $('.switch-cont');
+    
+    if ($(window).width() <= 991.98 && $s.hasClass('container')) {
+        $s.removeClass('container').addClass('container-fluid');
+    }
+    else if ($(window).width() > 991.98 && $s.hasClass('container-fluid')){
+        $s.removeClass('container-fluid').addClass('container');
+    }
+}
+
+// CLOSE MOBILE NAVIGATION WHEN THE BROWSERWINDOW IS BIGGER THAN PIXEL-NUMBER
+
+function openMenu() {
+    if($(window).width() > 991.98) {
+        $('.mobile-menu').removeClass('open-menu');
+    }
+}
+
 $(document).ready(function() {
+    
+    // ACCORDION FUNCTION TO OPEN ACCORDION AN REVEAL TEXT INSIDE
+    
    function close_accordion_section( $head, attr ) {
        $head.removeClass('active');
        $('.faq-accordion ' + attr).slideUp(300).removeClass('open');
@@ -17,16 +53,41 @@ $(document).ready(function() {
         e.preventDefault();
     });
     
-    if ($('body').height()<$(window).height()) {
-        $('.footer').addClass('fixed-footer');
-       }else {
-        $('.footer').removeClass('fixed-footer');
-       } 
+    // ADDING AN SPAN WITH THE GREATER-ICON IF THERE ARE MORE THEN ONE BREDCRUMB ON A SITE
     
     if ($('.breadcrumbs-container > a').length >= 2) {
         $('.breadcrumbs-container a:not(:last-child)').after('<span class="icon icon--greater before-crumbs"></span>');
     }
-    else {
-        console.log('');
-    }
+    
+    // OPEN NAVIGATION WHEN CLICK ON THE MENU-BUTTON
+    
+    $('.togglebutton').click(function(){
+       $('.mobile-menu').toggleClass('open-menu');
+    });
+    
+    // CLOSE NAVIGATION WHEN CLICK ON BACKDROP (Black background on the side of the menu)
+    
+    $('.backdrop').click(function(){
+       $('.mobile-menu').toggleClass('open-menu'); 
+    });
+    
+    setTimeout(function(){
+        fixFooter();
+    }, 200);
+    
+    containerSwitch();
+    
+    openMenu();
+    
+    // START FUNCTIONS WHEN RESIZING THE BROWSERWINDOW, THE FUNCTION BREAKS WHEN NO RESIZE
+    
+    $(window).resize(function(){
+
+    fixFooter();
+    
+    containerSwitch();
+        
+    openMenu();
+        
+    });
 });
